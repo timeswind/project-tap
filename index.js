8b9c252c42783b3569718c27bd2ffa29377e2134
Mingtian Yang <mky5089@psu.edu>
2020-03-15 08:51:53 +0800

    add next.js app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { Keystone } = require('@keystonejs/keystone');
 const { PasswordAuthStrategy } = require('@keystonejs/auth-password');
 const { GraphQLApp } = require('@keystonejs/app-graphql');
 const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { NextApp } = require('@keystonejs/app-next');
 const initialiseData = require('./initial-data');
 const UserScheme = require('./lists/user');
 const ProfileScheme = require('./lists/profile');
@@ -33,6 +34,8 @@ module.exports = {
     new AdminUIApp({
       enableDefaultRoute: true,
       authStrategy,
+      isAccessAllowed: ({ authentication: { item: user, listKey: list } }) => !!user && !!user.isAdmin,
     }),
+    new NextApp({ dir: 'app' }),
   ],
 };
